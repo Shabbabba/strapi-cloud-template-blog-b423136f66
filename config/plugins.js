c3a47b5b9f6f45389d8bc3968edd8889305cdfd5
Mark <info@tagtech-uk.com>
2025-09-09 21:33:58 +0100

    Switch email provider to SMTP (Hostinger)

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,20 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  email: {
+    config: {
+      provider: 'smtp',
+      providerOptions: {
+        host: env('SMTP_HOST'),
+        port: env.int('SMTP_PORT', 465),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+        secure: true, // SSL for port 465
+      },
+      settings: {
+        defaultFrom: 'info@tagtech-uk.com',
+        defaultReplyTo: 'info@tagtech-uk.com',
+      },
+    },
+  },
+});
